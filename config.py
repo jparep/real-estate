9860a3dd01ec616781c015d832a5b56d6ebb269a
jparep <parepson@gmail.com>
2024-08-25 13:46:21 -0600

    Added config dictionary to optimized config

--- config.py
+++ config.py
@@ -4,11 +4,25 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'defualt_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     MODEL_PATH = os.getenv('MODEL_PATH', 'models/model.joblib')
-    
+
+@staticmethod
+def init_app(app):
+    """Initialize the app with additional configurations.""" 
+    pass
+
 class DevelopmentConfig(Config):
     DEBUG=True
+    FLASK_ENV='development'
 
 class ProductionConfig(Config):
     DEBUG=False
+    FLASK_ENV = 'production'
+
+# Dictionary to map environment  to respective config class
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'defualt': DevelopmentConfig
+}
