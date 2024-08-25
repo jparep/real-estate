c1936b1055f441adea33d207bdb3bd23a523726b
jparep <parepson@gmail.com>
2024-08-25 15:23:27 -0600

    Updated wsgi file to use correct flask instance

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app
 
-# Determine hte environment from an environment variable, with 'production' as the default
-app = os.getenv('FLASK_CONFIG', 'production')
+# Determine the environment from an environment variable, with 'production' as the default
+config_name = os.getenv('FLASK_CONFIG', 'production')
 app = create_app(config_name)
