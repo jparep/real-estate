081661d36fe0b0164d82cd132ebf3cad704d89ba
jparep <parepson@gmail.com>
2024-08-25 14:05:24 -0600

    added register blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
+from config import Config
 
-def create_app():
+def create_app(config_name='defualt'):
     app= Flask(__name__)
-    app.config.from_object('config.Config')
     
-    from .routes import main
-    app.register_blueprint(main)
+    # Load the appropriate configuration
+    app.config.from_object(Config[config_name])
+    Config[config_name].init_app(app)
+    
+    # REgister your routes, blueprints, etc
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
     
     return app
