adb503f59502a19cdebfa28761f88456d7cf2bb9
jparep <parepson@gmail.com>
2024-08-24 21:34:57 -0600

    index method added

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,10 @@ from .models import model
 mian = Blueprint('main', __name__)
 
 @main.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        prediction = model(data)
+        return render_template('result.html')
+    render_template('index.html')
+
