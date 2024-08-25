cef60d17d55a522c1e2c436c61a2bfe0d7fed1e7
jparep <parepson@gmail.com>
2024-08-25 15:25:56 -0600

    Removed depreciated flask Script to FLask Migrate to manage migrations

--- manage.py
+++ manage.py
@@ -1,10 +1,14 @@
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from app import create_app
+from app import create_app, db  # Ensure you import the `db` object from your models
 
-# Specify the environment configuration
-app = create_app('production')
+config_name = os.getenv('FLASK_CONFIG', 'production')
+app = create_app(config_name)
 
 manager = Manager(app)
+migrate = Migrate(app, db)
 
-if __name__=='__main__':
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
     manager.run()
