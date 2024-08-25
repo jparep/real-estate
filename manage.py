e393519493287e1ff102a1268e7e2b11a873f6ff
jparep <parepson@gmail.com>
2024-08-25 15:41:05 -0600

    manage file updated to use flask migrate command to the manager

--- manage.py
+++ manage.py
@@ -1,14 +1,21 @@
-from flask_migrate import Migrate, MigrateCommand
+import os
 from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db  # Ensure you import the `db` object from your models
 
+# Determine the environment from an environment variable, with 'production' as the default
 config_name = os.getenv('FLASK_CONFIG', 'production')
 app = create_app(config_name)
 
+# Initialize Flask-Script manager
 manager = Manager(app)
+
+# Initialize Flask-Migrate
 migrate = Migrate(app, db)
 
+# Add Flask-Migrate commands to the manager
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
+    # Run the manager with the specified commands
     manager.run()
