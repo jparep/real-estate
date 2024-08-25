fcc390bf794c25edea3d37945f290e210cf6c5d3
jparep <parepson@gmail.com>
2024-08-24 21:50:16 -0600

    prediction method updated

--- app/models.py
+++ app/models.py
@@ -5,4 +5,5 @@ model = joblib.load('models/model.joblib')
 def predict_price(features):
     # Convert input data to the format expected by the model
     input_data = [float(features[key]) for key in features]
-    
+    prediction = model.predict([input_data])
+    return round(prediction[0], 2)
