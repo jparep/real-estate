1a1db510d0be4c9cef6e793ef3a9c33e472aa2c2
jparep <parepson@gmail.com>
2024-08-25 13:34:40 -0600

    Added DevelopmentConfig and ProductionConfig classes

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
 from dotenv import load_dotenv
 
+# Load environment variables from .env file
+
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'defualt_secret_key')
     MODEL_PATH = os.getenv('MODEL_PATH', 'models/model.joblib')
+    
+class DevelopmentConfig(Config):
+    DEBUG=True
+
+class ProductionConfig(Config):
+    DEBUG=False
