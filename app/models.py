5024d6d572fe1eaf100abd206736e24e6e2ae431
jparep <parepson@gmail.com>
2024-08-24 21:48:38 -0600

    predicted method created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 import joblib
 
 model = joblib.load('models/model.joblib')
+
+def predict_price(features):
+    # Convert input data to the format expected by the model
+    input_data = [float(features[key]) for key in features]
+    
