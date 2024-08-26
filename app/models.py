86d712f1d9562bbdd747071f2d55fe644bad594a
jparep <parepson@gmail.com>
2024-08-26 13:57:58 -0600

    Updated models file to use joblib instead of pickle

--- app/models.py
+++ app/models.py
@@ -1,12 +1,19 @@
-import pickle
+import joblib
 
 # Load the pre-trained model from the file
-with open('models/model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+with open('models/model.joblib', 'rb') as model_file:
+    model = joblib.load(model_file)
 
 def predict_price(features):
     """Predict the house price based on the input features."""
-    # Convert input data to the format expected by the model
-    input_data = [float(features[key]) for key in features]
+    # Extract and convert the input data to the format expected by the model
+    input_data = [
+        float(features['Avg. Area Income']),
+        float(features['Avg. Area House Age']),
+        float(features['Avg. Area Number of Rooms']),
+        float(features['Avg. Area Number of Bedrooms']),
+        float(features['Area Population'])
+    ]
+    
     prediction = model.predict([input_data])
     return round(prediction[0], 2)
